e19d9a984dad4030a7246af6ff62ec06960f8beb
Stefano Terna <stefano.terna@tomorrowdata.io>
2018-07-20 14:01:05 +0200

    add logging to get times and counts of messages

--- server/main.py
+++ server/main.py
@@ -1,11 +1,11 @@
 # Copyright 2018 TomorrowData Srl
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,26 +14,37 @@
 
 import json
 import logging
+from collections import Counter
+from datetime import datetime
 
 import tornado.ioloop
 from tornado.web import Application, RequestHandler, StaticFileHandler
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPError
 from tornado.websocket import WebSocketHandler
 
-logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(message)s'
+                    )
 
-
+message_counter = Counter(received = 0)
+first_request = {'timestamp': None}
 
 class UserWebHookHandler(RequestHandler):
     def post(self):
+        if not first_request['timestamp']:
+            first_request['timestamp'] = datetime.utcnow()
+
+        message_counter.update(received = 1)
+
         payload = json.loads(self.request.body)
-        logging.info(payload)
+        logging.info('{} - {} - {}'.format(first_request['timestamp'], payload['msg']['timestamp'], message_counter['received']))
 
         msg = {
             'device': payload['msg']['from'],
             'deviceTimestamp': payload['msg']['devicetimestamp'],
             'payload': payload['msg']['payload']
         }
+
         WebHookForwarder.forward_to_clients(msg)
 
 
